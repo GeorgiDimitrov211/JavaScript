502bdf9f452636830aa325392ea45478e30f8828
gecata21 <georgi.d.d97@gmail.com>
2018-02-08 18:46:14 +0100

    Changes to the code

--- JavaScript-Core/JavaScript-Advanced/01.DocumentObjectModel/07.SumTable.js
+++ JavaScript-Core/JavaScript-Advanced/01.DocumentObjectModel/07.SumTable.js
@@ -1,10 +1,11 @@
 function sum() {
 
   let sum = 0;
-  arr = document.getElementsByTagName("tr");
-
-  for (let i = 1; i < arr.length - 1; i++) {
-    sum += Number(arr[i].lastChild.textContent);
+  arr = document.getElementsByTagName("td");
+  console.log(arr)
+  for (let i = 1; i < arr.length-1; i+=2){
+    
+    sum += Number(arr[i].textContent);
   }
   document.getElementById('sum').textContent = sum;
 }
